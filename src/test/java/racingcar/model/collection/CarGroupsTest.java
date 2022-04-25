9e6ceda677198260705cce775427b8795dd5cc63
apio1130 <apio1130@naver.com>
2022-04-25 12:45:48 +0900

    feat(carGroups): car에 대한 일급 컬렉션 추가
    - car들에 대한 기능 메소드 추가
    - 테스트 코드 추가

--- src/test/java/racingcar/model/collection/CarGroupsTest.java
+++ src/test/java/racingcar/model/collection/CarGroupsTest.java
@@ -1,8 +1,10 @@
 package racingcar.model.collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 
 class CarGroupsTest {
 
@@ -20,5 +22,20 @@ class CarGroupsTest {
         assertThat(cars.getCars()).hasSize(1);
     }
 
+    @Test
+    void 자동차_이동_테스트() {
+        String carNames = "자동차1";
+        CarGroups carGroups = new CarGroups(carNames);
+
+        for (Car car : carGroups.getCars()) {
+            assertThat(car.getDrivingDistance().getDistance()).isZero();
+        }
+        for (int i = 0; i < 20; i++) {
+            carGroups.moveAll();
+        }
+        for (Car car : carGroups.getCars()) {
+            assertThat(car.getDrivingDistance().getDistance()).isNotZero();
+        }
+    }
 
 }
